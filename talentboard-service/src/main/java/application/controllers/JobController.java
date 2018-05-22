da6d48af1787d2b16820c2a4985e9280f47c651b
Hamza Basrai <hamza.basrai@gmail.com>
2018-05-21 23:57:38 -0400

    Added JobRespository to JobController

--- talentboard-service/src/main/java/application/controllers/JobController.java
+++ talentboard-service/src/main/java/application/controllers/JobController.java
@@ -1,47 +1,38 @@
 package application.controllers;
 
+import application.database.JobRepository;
 import application.models.Job;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class JobController {
 
-    List<Job> jobs = new ArrayList<>();
+    private final JobRepository repository;
+
+    public JobController(JobRepository repository) {
+        this.repository = repository;
+    }
 
     @GetMapping("/jobs")
     public List<Job> getJobs() {
-        return jobs;
+        return repository.findAll();
     }
 
     @GetMapping("/job/{id}")
     public Job getJob(@PathVariable String id) {
-        for (Job job: jobs) {
-            if (job.getId().equals(id)) {
-                return job;
-            }
-        }
-        return new Job();
+        return repository.findById(id).get();
     }
 
     @PostMapping("/job")
-    public Job addJob(@RequestBody Job newJob) {
-        jobs.add(newJob);
-        return newJob;
+    public Job addJob(@RequestBody Job job) {
+        return repository.save(job);
     }
 
     @DeleteMapping("/job/{id}")
-    public Job deleteJob(@PathVariable String id) {
-        for (Job job: jobs) {
-            if (job.getId().equals(id)) {
-                jobs.remove(job);
-                return job;
-            }
-        }
-
-        return new Job();
+    public void deleteJob(@PathVariable String id) {
+        repository.deleteById(id);
     }
 
 

224fc8a986e73c1f629dfce3a200bf14eebb9a08
Amar Jasarbasic <amarjasarbasic@gmail.com>
2018-05-23 02:37:16 -0400

    nav bar with semantic ui

--- talentboard-app/src/app/app.module.ts
+++ talentboard-app/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {SuiModule} from 'ng2-semantic-ui';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SuiModule
   ],
   providers: [],
   bootstrap: [AppComponent]

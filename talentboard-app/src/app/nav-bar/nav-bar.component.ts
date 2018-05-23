4ea1bdc163cc4029a12dd9286ac85687d1072ed8
Amar Jasarbasic <amarjasarbasic@gmail.com>
2018-05-23 02:37:16 -0400

    nav bar with semantic ui

---
+++ talentboard-app/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {SuiModule} from 'ng2-semantic-ui';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

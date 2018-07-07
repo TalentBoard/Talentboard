a6ca30d0d8113b89e9569f02011932f59ee09d9a
Amar Jasarbasic <amarjasarbasic@gmail.com>
2018-07-07 18:50:42 -0400

    merged nav-bar with master

--- talentboard-app/src/app/app.module.ts
+++ talentboard-app/src/app/app.module.ts
@@ -1,34 +1,5 @@
-<<<<<<< HEAD
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import {SuiModule} from 'ng2-semantic-ui';
-import { JobViewComponent } from './job-view/job-view.component';
-import { KanbanBoardComponent } from './kanban-board/kanban-board.component';
-import { CreateJobFormComponent } from './create-job-form/create-job-form.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    NavBarComponent,
-    JobViewComponent,
-    KanbanBoardComponent,
-    CreateJobFormComponent
-  ],
-  imports: [
-    BrowserModule,
-    SuiModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
-=======
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import {SuiModule} from 'ng2-semantic-ui';
@@ -66,4 +37,3 @@ import { AfService } from './providers/af.service';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
->>>>>>> nav-bar

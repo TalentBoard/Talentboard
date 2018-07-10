aed71a93bb878ba8d0367ddcadcd19b1d6c545e4
Hamza Basrai <hamza.basrai@gmail.com>
2018-07-10 15:33:24 -0400

    Removed unecessary else statement

--- talentboard-app/src/app/nav-bar/nav-bar.resolver.ts
+++ talentboard-app/src/app/nav-bar/nav-bar.resolver.ts
@@ -13,27 +13,25 @@ export class AppResolver implements Resolve<User> {
 
     return new Promise((resolve, reject) => {
       this.userService.getCurrentUser()
-      .then(res => {
-        if (res.providerData[0].providerId === 'password') {
-          user.id = res.uid;
-          user.profileURL = 'http://dsi-vd.github.io/patternlab-vd/images/fpo_avatar.png';
-          user.email = res.email;
-          user.name = res.displayName;
-          user.provider = res.providerData[0].providerId;
-          return resolve(user);
-        }
-        else {
+        .then(res => {
+          if (res.providerData[0].providerId === 'password') {
+            user.id = res.uid;
+            user.profileURL = 'http://dsi-vd.github.io/patternlab-vd/images/fpo_avatar.png';
+            user.email = res.email;
+            user.name = res.displayName;
+            user.provider = res.providerData[0].providerId;
+            return resolve(user);
+          }
           user.id = res.uid;
           user.profileURL = res.photoURL;
           user.name = res.displayName;
           user.provider = res.providerData[0].providerId;
           user.email = res.email;
           return resolve(user);
-        }
-      }, err => {
-        this.router.navigate(['/login']);
-        return reject(err);
-      });
+        }, err => {
+          this.router.navigate(['/login']);
+          return reject(err);
+        });
     });
   }
 }

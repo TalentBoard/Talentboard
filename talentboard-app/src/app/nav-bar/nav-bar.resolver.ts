88b78100800dcdf10b1b9d37cb8f4f36965e5794
Hamza Basrai <hamza.basrai@gmail.com>
2018-07-10 15:23:13 -0400

    Fixed breaking changes

--- talentboard-app/src/app/nav-bar/nav-bar.resolver.ts
+++ talentboard-app/src/app/nav-bar/nav-bar.resolver.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { UserService } from '../core/user.service';
-import { AccountUser } from '../model/User';
+import { User } from '../model/User';
 
 @Injectable()
-export class AppResolver implements Resolve<AccountUser> {
+export class AppResolver implements Resolve<User> {
   constructor(public userService: UserService, private router: Router) { }
 
-  resolve(route: ActivatedRouteSnapshot): Promise<AccountUser> {
+  resolve(route: ActivatedRouteSnapshot): Promise<User> {
 
-    const user = new AccountUser();
+    const user = new User();
 
     return new Promise((resolve, reject) => {
       this.userService.getCurrentUser()
       .then(res => {
         if (res.providerData[0].providerId === 'password') {
           user.id = res.uid;
-          user.profileImg = 'http://dsi-vd.github.io/patternlab-vd/images/fpo_avatar.png';
+          user.profileURL = 'http://dsi-vd.github.io/patternlab-vd/images/fpo_avatar.png';
           user.email = res.email;
           user.name = res.displayName;
           user.provider = res.providerData[0].providerId;
@@ -24,7 +24,7 @@ export class AppResolver implements Resolve<AccountUser> {
         }
         else {
           user.id = res.uid;
-          user.profileImg = res.photoURL;
+          user.profileURL = res.photoURL;
           user.name = res.displayName;
           user.provider = res.providerData[0].providerId;
           user.email = res.email;

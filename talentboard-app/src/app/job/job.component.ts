8392efba8dc89fcd3a504673dd7619429d9609a7
Amar Jasarbasic <amarjasarbasic@gmail.com>
2018-07-18 01:07:29 -0400

    added cards manually

--- talentboard-app/src/app/job/job.component.ts
+++ talentboard-app/src/app/job/job.component.ts
@@ -17,14 +17,22 @@ export interface IContext {
 export class JobComponent implements OnInit {
 
   currentJob: Job = new Job();
+  app1: Applicant = new Applicant();
+  app2: Applicant = new Applicant();
+  app3: Applicant = new Applicant();
+  app4: Applicant = new Applicant();
+  app5: Applicant = new Applicant();
+  app6: Applicant = new Applicant();
+  app7: Applicant = new Applicant();
+  app8: Applicant = new Applicant();
 
   jobList: Array<Job> = [];
   applicantList: Array<Applicant> = [];
-  applied: Array<Applicant> = [];
-  phoneInterview: Array<Applicant> = [];
-  personInterview: Array<Applicant> = [];
-  declined: Array<Applicant> = [];
-  offer: Array<Applicant> = [];
+  applied: Array<Applicant> = [this.app1, this.app2, this.app3];
+  phoneInterview: Array<Applicant> = [this.app4, this.app5];
+  personInterview: Array<Applicant> = [this.app6];
+  declined: Array<Applicant> = [this.app8];
+  offer: Array<Applicant> = [this.app7];
   locations = [
     'Calgary, AB',
     'Halifax, NS',
@@ -41,6 +49,32 @@ export class JobComponent implements OnInit {
   constructor(private modalService: SuiModalService, private jobService: JobService) { }
 
   ngOnInit() {
+
+    this.app1.name = 'John Smith';
+    this.app1.currentEmployer = 'Apple';
+    this.app1.currentTitle = 'Software Engineer';
+    this.app2.name = 'Andrew Zhang';
+    this.app2.currentEmployer = 'Google';
+    this.app2.currentTitle = 'Backend Engineer';
+    this.app3.name = 'Mike Smith';
+    this.app3.currentEmployer = 'Uber';
+    this.app3.currentTitle = 'Systems Engineer';
+    this.app4.name = 'Catherine Chow';
+    this.app4.currentEmployer = 'Airbnb';
+    this.app4.currentTitle = 'Software Engineer';
+    this.app5.name = 'Joe Smith';
+    this.app5.currentEmployer = 'Microsoft';
+    this.app5.currentTitle = 'Software Developer';
+    this.app6.name = 'Olivia Smith';
+    this.app6.currentEmployer = 'Apple';
+    this.app6.currentTitle = 'Software Engineer';
+    this.app7.name = 'Abdul Omar';
+    this.app7.currentEmployer = 'Samsung';
+    this.app7.currentTitle = 'Lead Software Engineer';
+    this.app8.name = 'Jamie Ryan';
+    this.app8.currentEmployer = 'Tesla';
+    this.app8.currentTitle = 'Software Engineer';
+
     this.jobService.getAllJobs().subscribe(jobs => {
       this.jobList = jobs;
       if (jobs[0] !== undefined && this.currentJob.id === undefined) {

ae265562355941b8ef89663aa711045c6a714e96
Hamza Basrai <hamza.basrai@gmail.com>
2018-10-09 18:58:34 -0400

    Deleted service project

--- talentboard-service/src/main/java/application/database/ApplicantRepository.java
+++
@@ -1,8 +0,0 @@
-package application.database;
-
-import application.models.Applicant;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ApplicantRepository extends MongoRepository<Applicant, String> {
-
-}

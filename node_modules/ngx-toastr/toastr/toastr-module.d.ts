0f24a2694a3a2d5275035f113a6abcebb0bcf0d8
Amar Jasarbasic <amar.jasarbasic@shopify.com>
2018-10-18 17:26:58 -0400

    ignore node_modules

--- node_modules/ngx-toastr/toastr/toastr-module.d.ts
+++
@@ -1,6 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-import { GlobalConfig } from './toastr-config';
-export declare class ToastrModule {
-    constructor(parentModule: ToastrModule);
-    static forRoot(config?: Partial<GlobalConfig>): ModuleWithProviders;
-}
